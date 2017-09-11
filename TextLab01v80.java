cbcc2246905e7cd7c0e05746e1b7967ee2da2069
919691 <31707446+919691@users.noreply.github.com>
2017-09-11 11:27:30 -0600

    Add files via upload

---
+++ TextLab01v80.java
@@ -0,0 +1,21 @@
+/*TextLab01v80.java
+ *Christopher Lawson pd. 5
+ *The 80-point version of Text Lab 01 is an exercise in precisely copying a
+ *provided program, followed by compiling the source code into bytecode
+ *and executing the byte code file.
+ */
+
+class TextLab01v80 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Text Lab 01, 80 Point Version\n\n");
+		System.out.print("Measure today's knowledge ");
+		System.out.println("by yesterday's confusion.\n");
+		System.out.println("Bewilderment + Exposure = Obvious\n");
+		System.out.println("# ##### #####     ##### ##### # #   # ##### #####");
+		System.out.println("# #   # #   #     #   # #   # # ##  #   #   #    ");
+		System.out.println("# #   # #   #)
+
+		}
+}
